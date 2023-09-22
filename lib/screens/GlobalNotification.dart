c5afa087c3f82282197946a9d6dad1afce99fa53
Divyanshu <divaibhavyanshu@gmail.com>
2023-09-23 04:57:51 +0530

    created Global Notification

--- lib/screens/GlobalNotification.dart
+++ lib/screens/GlobalNotification.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class GlobalNotification extends StatelessWidget {
@@ -5,8 +7,46 @@ class GlobalNotification extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("mere dil me jagah")),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('GlobalNotification')
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Global Notifications"),
+          ),
+          body: ListView(
+            shrinkWrap: true,
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data =
+                  document.data()! as Map<String, dynamic>;
+
+              // appBar: AppBar(
+              //   title: Text("Global Notification"),
+              // ),
+              return Column(
+                // mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    // height: 100,
+                    child: CachedNetworkImage(imageUrl: data["image"]),
+                  ),
+                  Divider()
+                ],
+              );
+            }).toList(),
+          ),
+        );
+      },
     );
   }
 }
